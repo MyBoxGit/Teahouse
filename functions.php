11910ac42d47a133093ef9be31bff8b7035d430b
MyBoxGit <759046501@qq.com>
2015-11-01 18:23:39 +0800

    update

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * Island 功能和定义
  *
@@ -449,14 +449,17 @@ function getPostViews($postID){
 function setPostViews($postID) {
     $count_key = 'post_views_count';
     $count = get_post_meta($postID, $count_key, true);
-    if($count==''){
-        $count = 0;
-        delete_post_meta($postID, $count_key);
-        add_post_meta($postID, $count_key, '0');
-    }else{
-        $count++;
-        update_post_meta($postID, $count_key, $count);
+    if (is_singular()) {
+        if($count==''){
+            $count = 0;
+            delete_post_meta($postID, $count_key);
+            add_post_meta($postID, $count_key, '0');
+        }else{
+            $count++;
+            update_post_meta($postID, $count_key, $count);
+        }
     }
+    
 }
 
 
